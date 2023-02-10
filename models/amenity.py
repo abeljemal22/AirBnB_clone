f992e7fc31918801da65786f47cadf884c5f05d8
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-02-10 16:11:19 +0300

    airbnb

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,11 @@
 """ holds class Amenity"""
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     """Representation of Amenity """
     name = ""
-    
+
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
         super().__init__(*args, **kwargs)
